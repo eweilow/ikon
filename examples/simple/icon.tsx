699175c55eea03ce48bc85508b146bf24193326a
E. Weilow <eweilow@digitalkoppar.se>
2019-05-25 21:21:50 +0200

    Nicer example icon

--- examples/simple/icon.tsx
+++ examples/simple/icon.tsx
@@ -3,7 +3,14 @@ import { IconGenerationComponent } from "@eweilow/ikon-cli";
 
 const Component: IconGenerationComponent = props => (
   <div
-    style={{ background: "white", color: "red", height: "100%", width: "100%" }}
+    style={{
+      background:
+        "linear-gradient(155deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+      color: "#5afd1d",
+      fontWeight: "bold",
+      height: "100%",
+      width: "100%"
+    }}
   >
     {props.name}: {props.width}x{props.height}@
     {(props.pixelRatio * 100).toFixed(0)}%
