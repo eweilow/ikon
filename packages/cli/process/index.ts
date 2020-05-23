16e3a72f95f4602effabf6180e9dc69689121004
eweilow <eweilow@digitalkoppar.se>
2020-05-23 15:22:13 +0200

    Compile more files

--- packages/cli/process/index.ts
+++ packages/cli/process/index.ts
@@ -1,14 +1,14 @@
-import { buildProcess, htmlProcess } from "./build";
-import { devProcess } from "./dev";
-
 async function main() {
   const args = JSON.parse(process.env!.IKON_ARGS || "{}");
 
   if ((process.env.mode as string) === "watch") {
+    const { devProcess } = require("../dist/cjs/process/dev");
     await devProcess(args);
   } else if ((process.env.mode as string) === "build") {
+    const { buildProcess } = require("../dist/cjs/process/build");
     await buildProcess(args);
   } else if ((process.env.mode as string) === "html") {
+    const { htmlProcess } = require("../dist/cjs/process/build");
     await htmlProcess(args);
   } else {
     throw new Error(`Unknown mode: '${process.env.mode}'`);
